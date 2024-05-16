7de6c2de26ac9cf9d81c7a55be4be11b68ee103a
keeganmhawkins@gmail.com <keeganmhawkins@gmail.com>
2024-05-16 11:57:34 -0500

    Light/Dark mode working

--- fascar/src/App.jsx
+++ fascar/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
-
+import { useState } from 'react';
 
 function App() {
+  const [light, setLight] = useState(true)
+
+  const changeMode = (light) => {
+    setLight(light)
+    console.log(light)
+  }
 
   return (
-    <div className='App'>
-      <Navbar/>
+    <div className='App' data-theme={light ? 'light' : 'dark'}>
+      <Navbar changeMode={changeMode}/>
       <Home/>
     </div>
   )
--- fascar/src/components/Navbar.jsx
+++ fascar/src/components/Navbar.jsx
@@ -1,25 +1,35 @@
-import Fascar from '../assets/Images/FascarLogo.svg';
+import dfascar from '../assets/Images/FascarLogo.png';
+import lfascar from '../assets/Images/lfascar.png';
 import { HiOutlineMagnifyingGlass, HiMoon, HiSun } from 'react-icons/hi2';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Navbar() {
-    const [light, setLight] = useState(false)
+function Navbar( { changeMode } ) {
+    const [light, setLight] = useState(true);
 
+    const handleMode = () => {
+        setLight(!light)
+        changeMode(light)
+    }
   return (
     <div className='navbar'>
         <div className='logo-container'>
-            <img className='logo-pic' src={Fascar} alt='Fascar Logo' width={70} height={70}/>
+            <img className='logo-pic' src={light? dfascar: lfascar} alt='Fascar Logo' width={70} height={70}/>
             <h1 className='logo-name'>Fascar</h1>
         </div>
         <div className="search-bar-container">
-            <HiOutlineMagnifyingGlass/>
-            <input type="text" />
+            <HiOutlineMagnifyingGlass className='search-icon'/>
+            <input type="text" placeholder='Search Cars'/>
         </div>
         <div className='light-dark-mode-container'>
-            {light?<HiMoon onClick={() => setLight(!light)}/>:<HiSun onClick={() => setLight(!light)}/>}
+            {light ? <HiMoon onClick={() => handleMode(light)}/>:<HiSun onClick={() => handleMode(!light)}/>}
         </div>
     </div>
   )
+}
+
+Navbar.propTypes = {
+    changeMode: PropTypes.func
 }
 
 export default Navbar
