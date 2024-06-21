95cc1d85311544cd6a82e114084800229dba2ef2
keeganmhawkins@gmail.com <keeganmhawkins@gmail.com>
2024-06-21 11:19:56 -0500

    Changed allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://fascar.onrender.com',
+    'https://fascar-api.onrender.com'
 ]
 
 module.exports = allowedOrigins
