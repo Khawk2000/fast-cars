b285dca91133ff1914acccf453f0ea4f7f1a267a
keeganmhawkins@gmail.com <keeganmhawkins@gmail.com>
2024-06-24 13:10:23 -0500

    loading errors

--- fascar/src/pages/Home.jsx
+++ fascar/src/pages/Home.jsx
@@ -28,6 +28,7 @@ function Home() {
       </section>
       <section className="all-cars-with-filters">
         <h1 className="car-list-title">Best Rental Cars this side of the Mississippi</h1>
+        {!dcars && <h1>LOADING...</h1>}
         {dcars && <ul className="car-cards">{dcars.filter(dcar => dcar.make.includes(text) || dcar.make.toLowerCase().includes(text) || dcar.model.includes(text) || dcar.model.toLowerCase().includes(text) || dcar.year.toLowerCase().includes(text)).map(dcar =>
           <CarCard key={dcar._id} make={dcar.make} model={dcar.model} src={dcar.pic} year={dcar.year} path={`/${dcar._id}`}/>
         )}
